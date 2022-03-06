5670f161ca1ab4cfeb646f185575b81dc3d96a54
hutamatr <hutamatr@gmail.com>
2022-03-06 18:48:02 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <section className="container">
+        <h1 className="text-2xl text-white">Lower sugar recipes</h1>
+        <p>
+          As we go about our hectic, busy days, it can be very easy to resort to
+          a sugary snack midway through the afternoon to keep your energy levels
+          elevated.
+        </p>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
