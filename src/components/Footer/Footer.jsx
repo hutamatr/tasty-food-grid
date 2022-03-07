64c9bda1a3f0f09c78ed8472e8730eeed09fa406
hutamatr <hutamatr@gmail.com>
2022-03-07 14:12:10 +0700

    Add link on footer & change title

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,23 @@ const Footer = () => {
   return (
     <footer>
       <section className="text-center text-xs flex flex-col text-slate-800 py-8">
-        <p>Challenge from Waveast Discord Group.</p>
+        <p>
+          Challenge from{" "}
+          <a
+            href="https://azhariemuhammad.notion.site/1-Resto-app-df14ffa3b0e34469a71d8ee3f7f83a6c"
+            className="text-lime-700"
+          >
+            Waveast
+          </a>{" "}
+          Discord Group.
+        </p>
         <div className="flex flex-row justify-center items-center">
           Make with{" "}
           <img src={Love} alt="love" className="w-5 mx-1 animate-pulse" />
-          by<a href="https://twitter.com/huutamatr">hutamatr</a>
+          by
+          <a href="https://twitter.com/huutamatr" className="text-lime-700">
+            hutamatr
+          </a>
         </div>
       </section>
     </footer>
